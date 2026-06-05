e17ee11fd93f355123050ec08cb22dfd99552c33
E Harrison <ethan.harrison111111@gmail.com>
2026-06-05 14:07:35 -0700

    wandb plotting

---
+++ run_paths.py
@@ -0,0 +1,66 @@
+legend_to_run_paths = {
+      "Naive ID=10": [
+        "eharrison-university-of-california-berkeley/DSRL_pi0_InferenceDelay_Aloha_Full/dsrl_pi0_aloha_utd25_2026_05_25_14_19_50_0000--s-0_qf25_id10",
+        "eharrison-university-of-california-berkeley/DSRL_pi0_InferenceDelay_Aloha_Full/dsrl_pi0_aloha_utd25_2026_05_25_14_19_54_0000--s-1_qf25_id10",
+        "eharrison-university-of-california-berkeley/DSRL_pi0_InferenceDelay_Aloha_Full/dsrl_pi0_aloha_utd25_2026_05_25_14_19_57_0000--s-2_qf25_id10",
+        "eharrison-university-of-california-berkeley/DSRL_pi0_InferenceDelay_Aloha_Full/dsrl_pi0_aloha_utd25_2026_05_26_03_10_40_0000--s-3_qf25_id10",
+        "eharrison-university-of-california-berkeley/DSRL_pi0_InferenceDelay_Aloha_Full/dsrl_pi0_aloha_utd25_2026_05_26_03_10_44_0000--s-4_qf25_id10",
+        "eharrison-university-of-california-berkeley/DSRL_pi0_InferenceDelay_Aloha_Full/dsrl_pi0_aloha_utd25_2026_05_26_03_10_44_0000--s-5_qf25_id10"
+      ],
+      "Naive ID=10 (PA)": [
+        "eharrison-university-of-california-berkeley/DSRL_pi0_InferenceDelay_Aloha_Full/dsrl_pi0_aloha_utd25_2026_05_25_14_12_01_0000--s-0_qf25_id10_pca",
+        "eharrison-university-of-california-berkeley/DSRL_pi0_InferenceDelay_Aloha_Full/dsrl_pi0_aloha_utd25_2026_05_25_14_12_07_0000--s-1_qf25_id10_pca",
+        "eharrison-university-of-california-berkeley/DSRL_pi0_InferenceDelay_Aloha_Full/dsrl_pi0_aloha_utd25_2026_05_25_14_12_11_0000--s-2_qf25_id10_pca",
+        "eharrison-university-of-california-berkeley/DSRL_pi0_InferenceDelay_Aloha_Full/dsrl_pi0_aloha_utd25_2026_05_26_03_13_18_0000--s-3_qf25_id10_pca",
+        "eharrison-university-of-california-berkeley/DSRL_pi0_InferenceDelay_Aloha_Full/dsrl_pi0_aloha_utd25_2026_05_26_03_13_18_0000--s-4_qf25_id10_pca",
+        "eharrison-university-of-california-berkeley/DSRL_pi0_InferenceDelay_Aloha_Full/dsrl_pi0_aloha_utd25_2026_05_26_03_13_17_0000--s-5_qf25_id10_pca"
+      ],
+      "Naive ID=10 (US)": [
+        "eharrison-university-of-california-berkeley/DSRL_pi0_InferenceDelay_Aloha_Full/dsrl_pi0_aloha_utd25_2026_05_25_14_08_58_0000--s-0_qf25_id10_rld5",
+        "eharrison-university-of-california-berkeley/DSRL_pi0_InferenceDelay_Aloha_Full/dsrl_pi0_aloha_utd25_2026_05_25_14_09_01_0000--s-1_qf25_id10_rld5",
+        "eharrison-university-of-california-berkeley/DSRL_pi0_InferenceDelay_Aloha_Full/dsrl_pi0_aloha_utd25_2026_05_25_14_09_01_0000--s-2_qf25_id10_rld5",
+        "eharrison-university-of-california-berkeley/DSRL_pi0_InferenceDelay_Aloha_Full/dsrl_pi0_aloha_utd25_2026_05_26_03_13_58_0000--s-3_qf25_id10_rld5",
+        "eharrison-university-of-california-berkeley/DSRL_pi0_InferenceDelay_Aloha_Full/dsrl_pi0_aloha_utd25_2026_05_26_03_13_58_0000--s-4_qf25_id10_rld5",
+        "eharrison-university-of-california-berkeley/DSRL_pi0_InferenceDelay_Aloha_Full/dsrl_pi0_aloha_utd25_2026_05_26_03_13_58_0000--s-5_qf25_id10_rld5"
+      ],
+      "Naive ID=10 (PA, US)": [
+        "eharrison-university-of-california-berkeley/DSRL_pi0_InferenceDelay_Aloha_Full/dsrl_pi0_aloha_utd25_2026_05_25_14_09_09_0000--s-0_qf25_id10_rld5_pca",
+        "eharrison-university-of-california-berkeley/DSRL_pi0_InferenceDelay_Aloha_Full/dsrl_pi0_aloha_utd25_2026_05_25_14_09_09_0000--s-1_qf25_id10_rld5_pca",
+        "eharrison-university-of-california-berkeley/DSRL_pi0_InferenceDelay_Aloha_Full/dsrl_pi0_aloha_utd25_2026_05_25_14_12_01_0000--s-2_qf25_id10_rld5_pca",
+        "eharrison-university-of-california-berkeley/DSRL_pi0_InferenceDelay_Aloha_Full/dsrl_pi0_aloha_utd25_2026_05_26_03_13_58_0000--s-3_qf25_id10_rld5_pca",
+        "eharrison-university-of-california-berkeley/DSRL_pi0_InferenceDelay_Aloha_Full/dsrl_pi0_aloha_utd25_2026_05_26_03_13_58_0000--s-4_qf25_id10_rld5_pca",
+        "eharrison-university-of-california-berkeley/DSRL_pi0_InferenceDelay_Aloha_Full/dsrl_pi0_aloha_utd25_2026_05_26_03_13_58_0000--s-5_qf25_id10_rld5_pca"
+      ],
+      "Naive ID=20": [
+        "eharrison-university-of-california-berkeley/DSRL_pi0_InferenceDelay_Aloha_Full/dsrl_pi0_aloha_utd25_2026_05_25_14_12_21_0000--s-0_qf25_id20",
+        "eharrison-university-of-california-berkeley/DSRL_pi0_InferenceDelay_Aloha_Full/dsrl_pi0_aloha_utd25_2026_05_25_14_12_22_0000--s-1_qf25_id20",
+        "eharrison-university-of-california-berkeley/DSRL_pi0_InferenceDelay_Aloha_Full/dsrl_pi0_aloha_utd25_2026_05_25_14_12_20_0000--s-2_qf25_id20",
+        "eharrison-university-of-california-berkeley/DSRL_pi0_InferenceDelay_Aloha_Full/dsrl_pi0_aloha_utd25_2026_05_26_03_10_44_0000--s-3_qf25_id20",
+        "eharrison-university-of-california-berkeley/DSRL_pi0_InferenceDelay_Aloha_Full/dsrl_pi0_aloha_utd25_2026_05_26_03_10_44_0000--s-4_qf25_id20",
+        "eharrison-university-of-california-berkeley/DSRL_pi0_InferenceDelay_Aloha_Full/dsrl_pi0_aloha_utd25_2026_05_26_03_10_44_0000--s-5_qf25_id20"
+      ],
+      "Naive ID=20 (PA)": [
+        "eharrison-university-of-california-berkeley/DSRL_pi0_InferenceDelay_Aloha_Full/dsrl_pi0_aloha_utd25_2026_05_25_14_12_11_0000--s-0_qf25_id20_pca",
+        "eharrison-university-of-california-berkeley/DSRL_pi0_InferenceDelay_Aloha_Full/dsrl_pi0_aloha_utd25_2026_05_25_14_12_20_0000--s-1_qf25_id20_pca",
+        "eharrison-university-of-california-berkeley/DSRL_pi0_InferenceDelay_Aloha_Full/dsrl_pi0_aloha_utd25_2026_05_25_14_12_16_0000--s-2_qf25_id20_pca",
+        "eharrison-university-of-california-berkeley/DSRL_pi0_InferenceDelay_Aloha_Full/dsrl_pi0_aloha_utd25_2026_05_26_03_13_17_0000--s-3_qf25_id20_pca",
+        "eharrison-university-of-california-berkeley/DSRL_pi0_InferenceDelay_Aloha_Full/dsrl_pi0_aloha_utd25_2026_05_26_03_13_17_0000--s-4_qf25_id20_pca",
+        "eharrison-university-of-california-berkeley/DSRL_pi0_InferenceDelay_Aloha_Full/dsrl_pi0_aloha_utd25_2026_05_26_03_13_17_0000--s-5_qf25_id20_pca"
+      ],
+      "Naive ID=20 (US)": [
+        "eharrison-university-of-california-berkeley/DSRL_pi0_InferenceDelay_Aloha_Full/dsrl_pi0_aloha_utd25_2026_05_25_14_12_19_0000--s-0_qf25_id20_rld10",
+        "eharrison-university-of-california-berkeley/DSRL_pi0_InferenceDelay_Aloha_Full/dsrl_pi0_aloha_utd25_2026_05_25_14_12_19_0000--s-1_qf25_id20_rld10",
+        "eharrison-university-of-california-berkeley/DSRL_pi0_InferenceDelay_Aloha_Full/dsrl_pi0_aloha_utd25_2026_05_25_14_12_19_0000--s-2_qf25_id20_rld10",
+        "eharrison-university-of-california-berkeley/DSRL_pi0_InferenceDelay_Aloha_Full/dsrl_pi0_aloha_utd25_2026_05_26_03_14_05_0000--s-3_qf25_id20_rld10",
+        "eharrison-university-of-california-berkeley/DSRL_pi0_InferenceDelay_Aloha_Full/dsrl_pi0_aloha_utd25_2026_05_26_03_14_05_0000--s-4_qf25_id20_rld10",
+        "eharrison-university-of-california-berkeley/DSRL_pi0_InferenceDelay_Aloha_Full/dsrl_pi0_aloha_utd25_2026_05_26_03_14_04_0000--s-5_qf25_id20_rld10"
+      ],
+      "Naive ID=20 (PA, US)": [
+        "eharrison-university-of-california-berkeley/DSRL_pi0_InferenceDelay_Aloha_Full/dsrl_pi0_aloha_utd25_2026_05_25_14_12_19_0000--s-0_qf25_id20_rld10_pca",
+        "eharrison-university-of-california-berkeley/DSRL_pi0_InferenceDelay_Aloha_Full/dsrl_pi0_aloha_utd25_2026_05_25_14_12_19_0000--s-1_qf25_id20_rld10_pca",
+        "eharrison-university-of-california-berkeley/DSRL_pi0_InferenceDelay_Aloha_Full/dsrl_pi0_aloha_utd25_2026_05_25_14_12_18_0000--s-2_qf25_id20_rld10_pca",
+        "eharrison-university-of-california-berkeley/DSRL_pi0_InferenceDelay_Aloha_Full/dsrl_pi0_aloha_utd25_2026_05_26_03_14_04_0000--s-3_qf25_id20_rld10_pca",
+        "eharrison-university-of-california-berkeley/DSRL_pi0_InferenceDelay_Aloha_Full/dsrl_pi0_aloha_utd25_2026_05_26_03_14_04_0000--s-4_qf25_id20_rld10_pca",
+        "eharrison-university-of-california-berkeley/DSRL_pi0_InferenceDelay_Aloha_Full/dsrl_pi0_aloha_utd25_2026_05_26_03_14_04_0000--s-5_qf25_id20_rld10_pca"
+      ],
+}
